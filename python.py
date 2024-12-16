61e7331fa19ad348bef45ff2b0400e6d95aee087
Hemanthk408 <g.hemanth0408@gmail.com>
2024-12-16 22:56:50 +0530

    Update python.py

--- python.py
+++ python.py
@@ -5,4 +5,21 @@ df=pd.read_csv("https://github.com/Hemanthk408/Cars-sales-data/blob/main/Car_sal
 #print(df) or only type df for showing the table
 df
 
+#After that cleaning the data
+# Check the first few rows for columns title
+print(df.head())
 
+# Summary of the dataset
+print(df.info())
+
+# Check for missing values if any 0 or NAN value are in the data this code will show the count of that
+print(df.isnull().sum())
+
+## Fill missing values with a default value
+df['ColumnName'] = df['ColumnName'].fillna('DefaultValue')
+
+# Drop rows with missing values
+df = df.dropna()
+
+# Data is have any duplicates this code will drop the duplicate in this code keep=first is keep the first duplicte with out droping
+df = df.drop_duplicates(keep="first")
